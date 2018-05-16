ef347a12fa045239f5e7ddbdf002df3a3bffd946
Leo Taylor <leotaylor723@gmail.com>
2018-05-15 21:16:43 -0500

    exs and locations print to dom

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,25 +1,64 @@
-const loadEx = require('./ex');
-const loadLocations = require('./locations');
+// const loadEx = require('./ex');
+// const loadLocations = require('./locations');
 const print = require('./dom');
-const bindEvents = require('./events');
+// const bindEvents = require('./events');
 
-const whenExLoads = (data) => {
-  $('.exDiv').append(print.writeEx(data.crazyEx));
-  bindEvents();
+// const whenExLoads = (data) => {
+//   $('.exDiv').append(print.writeEx(data.crazyEx));
+//   bindEvents();
+// };
+
+// const whenExDontLoad = (error) => {
+//   console.error('failure');
+// };
+
+// const whenLocationsLoad = (data) => {
+//   console.log('locationData: ', data);
+//   $('.locationsDiv').append(print.writeLocal(data.Locations));
+// };
+
+// const initializer = () => {
+//   loadEx(whenExLoads, whenExDontLoad);
+//   loadLocations(whenLocationsLoad, whenExDontLoad);
+// };
+
+const exJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.get('../db/ex.json')
+      .done((data) => {
+        resolve(data.crazyEx);
+      })
+      .fail((err) => {
+        reject('you suck at this', err);
+      });
+  });
 };
 
-const whenExDontLoad = (error) => {
-  console.error('failure');
+const locationsJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.get('../db/locations.json')
+      .done((data) => {
+        resolve(data.Locations);
+      })
+      .fail((err) => {
+        reject('you suck at this', err);
+      });
+  });
 };
 
-const whenLocationsLoad = (data) => {
-  console.log('locationData: ', data);
-  $('.locationsDiv').append(print.writeLocal(data.Locations));
+const printAllData = () => {
+  exJSON().then((results) => {
+    print.writeEx(results);
+    return locationsJSON();
+  }).then((results) => {
+    print.writeLocal(results);
+  }).catch((error) => {
+    console.error('error', error);
+  });
 };
 
 const initializer = () => {
-  loadEx(whenExLoads, whenExDontLoad);
-  loadLocations(whenLocationsLoad, whenExDontLoad);
+  printAllData();
 };
 
 module.exports = initializer;
